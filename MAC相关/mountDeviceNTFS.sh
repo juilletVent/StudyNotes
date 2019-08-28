b026dda778d69252df916eea7dea85e93b92365f
和风柒月 <971616154@qq.com>
2019-08-28 22:02:03 +0800

    Update mountDeviceNTFS.sh

--- MAC相关/mountDeviceNTFS.sh
+++ MAC相关/mountDeviceNTFS.sh
@@ -2,8 +2,8 @@
 ###
 # @Author: WeiHong Ran
 # @Date: 2019-08-27 17:16:08
-# @LastEditors: Gmsoft - WeiHong Ran
-# @LastEditTime: 2019-08-28 19:13:19
+ # @LastEditors: WeiHong Ran
+ # @LastEditTime: 2019-08-28 21:59:25
 # @Description: Nothing
 ###
 
@@ -13,6 +13,7 @@ diskutil list
 read -p "请输入磁盘名称：" deviceName
 
 # 挂载磁盘
-sudo cat deviceName none ntfs rw,auto,nobrowse >>/etc/fstab
+sudo echo LABEL=$deviceName none ntfs rw,auto,nobrowse >>/etc/fstab
+
 # 创建连接
-sudo ln -s /Volumes/deviceName ~/Desktop/deviceName
+sudo ln -s /Volumes/$deviceName ~/Desktop/$deviceName
