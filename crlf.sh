0bd6a1a3349f947c3e2247aeb49301a005eafefa
和风柒月 <971616154@qq.com>
2019-09-12 11:31:41 +0800

    Update crlf.sh

--- crlf.sh
+++ crlf.sh
@@ -1,1 +1,9 @@
+
+# 全部转换
 find ./ -type f -print0 | xargs -0 dos2unix
+# 忽略目录
+# find . -path ./node_modules -prune -o -type f -print0 | xargs -0 dos2unixfind
+# find . -path ./node_modules -o -path ./build -prune -o -type f -print0 | xargs -0 dos2unixfind
+# 关键部分： -path ./node_modules -prune -o
+# -path 后接忽略的目录即可
+# 多目录忽略 -path ./node_modules -o -path ./build -prune -o  重复书写-path ./build -prune -o即可
