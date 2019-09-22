4c020b8d4447fc7c1af0cd154f76e54d25b9baae
和风柒月 <971616154@qq.com>
2019-09-22 11:32:24 +0800

    Update CharsetFilter.java

--- Java/TestProject/MessageBook/src/cn/nanami52/message/filter/CharsetFilter.java
+++ Java/TestProject/MessageBook/src/cn/nanami52/message/filter/CharsetFilter.java
@@ -1,3 +1,10 @@
+/*
+ * @Author: WeiHong Ran
+ * @Date: 2019-09-21 00:06:17
+ * @LastEditors: WeiHong Ran
+ * @LastEditTime: 2019-09-22 11:28:06
+ * @Description: Nothing
+ */
 package cn.nanami52.message.filter;
 
 import javax.servlet.*;
@@ -11,6 +18,7 @@ public class CharsetFilter implements Filter {
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
         chain.doFilter(req, resp);
     }
 
