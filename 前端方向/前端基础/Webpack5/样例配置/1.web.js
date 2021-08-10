041bf347ed5abaf961e7e58e885af1225cbe5cf6
juilletVent <julysunshine0914@gmail.com>
2021-08-10 17:38:53 +0800

    Update 1.web.js

--- 前端方向/前端基础/Webpack5/样例配置/1.web.js
+++ 前端方向/前端基础/Webpack5/样例配置/1.web.js
@@ -105,4 +105,34 @@ module.exports = {
       root: "React",
     },
   },
+  optimization: {
+    minimize: false,
+    // 创建在所有chunk间共享的runtime模块
+    runtimeChunk: "single",
+    // 运行环境模式，用于设置process.env.NODE_ENV的值，
+    nodeEnv: "production",
+    // 模块id命名模式
+    chunkIds: "deterministic",
+    // 使用非默认的压缩处理插件
+    // minimizer: [
+    //   new TerserPlugin({
+    //     parallel: true,
+    //     terserOptions: {
+    //       // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
+    //     },
+    //   }),
+    // ],
+    // 导入模块配置，对于懒加载与动态导入非常有用
+    // splitChunks: {},
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      // Definitions...
+    }),
+  ],
+  // 生产环境下会缓存编译结果加快构建速度
+  cache: true,
+  externals: {
+    jquery: "jQuery",
+  },
 };
